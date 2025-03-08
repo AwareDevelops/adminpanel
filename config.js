527354d9311adb7262176172310b024f7f76a7ed
AwareDevelops <pizzazz_84_batsmen@icloud.com>
2025-03-08 00:25:18 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    discord: {
+        clientID: '1289031892620017715',
+        clientSecret: 'wilda9dCxHpC-EaUca51RZs2CvagND75',
+        callbackURL: 'https://uhaware.tech/auth/discord/callback',
+        scope: ['identify', 'email', 'guilds']
+    },
+    mongoURI: 'mongodb+srv://admin:JkwHafwpVpkgYW5W@cluster0.suhcc.mongodb.net' 
+};
